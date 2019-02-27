a5f5300627d7e0e254673c1b06d27b2ff4b7ca5d
PPadilla99 <pedropadillamolina@gmail.com>
2019-02-27 18:18:15 +0100

    añado index registro y archivo css vinculado

---
+++ pruebaweb/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="styleindex.css">
+    <title>La Rasa</title>
+</head>
+<body>
+    <header><h1>Bienvenido a la Autoescuela La Rasa</h1></header>
+    <form action="/action_page.php" method="post">
+    <main><fieldset>
+    <legend><h2>Inicia Sesión:</h2></legend>
+    <strong>Usuario: </strong><input type="text" name="Nombre" maxlength="15">
+    <p><strong>Contraseña: </strong><input type="text" name="Nombre" maxlength="15"></p>
+    <p><input type="submit" value="Confirmar"></p>
+    <p><a href="registro.php">No tienes cuenta, Registrate</a></p>
+    </fieldset>
+    </form>
+    </main>
+
+
+
+</body>
+</html>
