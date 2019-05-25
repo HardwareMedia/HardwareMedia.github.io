96049478610e8a58f800ca366b0e949e18f522fc
Pedro Padilla <pedropadillamolina@gmail.com>
2019-05-25 23:55:53 +0200

    25/05 23:55

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<header>
+            <a href="index.php"><img class="imagenLogo" src="images/logo.jpg"
+       alt="HardwareMedia"></a>
+            <a href="index.php"><img class="casita" src="images/casa.png" alt="Inicio"></a>
+       <nav class="barraSuperior">
+        <ul>
+            <li><a href="#">Noticias</a>
+                <ul>
+                    <li><a href="juegos.php">Videojuegos</a></li>
+                    <li><a href="hardware.php">Hardware</a></li>
+                </ul></li>
+            <li><a href="#">PC</a>
+                <ul>
+                    <li><a href="mantenimiento.php">Mantenimiento</a></li>
+                    <li><a href="montaje.php">Montaje</a></li>
+                    <li><a href="modding.php">Modding</a></li>
+                    </ul></li>
+            <li><a href="#">Información</a>
+                <ul>
+                    <li><a href="nosotros.php">Nosotros</a></li>
+                    <li><a href="contactanos.php">Contáctanos</a></li>
+                    <li><a href="proyectos.php">Proyectos</a></li>
+                    <li><a href="asesoramiento.php">Asesoramiento</a></li>
+                </ul></li>
+            <li><a href="tienda.php">Tienda</a></li>    
+            <li><a href="https://twitter.com/hardwaremedia_?lang=es" target="_blank"><img class="twitter" src="images/twitter icon.png"></a></li>
+            <li><a href="https://www.instagram.com/hardwaremedia_/?hl=es" target="_blank"><img class="twitter" src="images/instagram.png"></a></li>
+        </ul>
+    </nav>          
+    </header>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="es">
+<!DOCTYPE php>
+<php lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,30 +21,30 @@
 
 
     <header>
-            <a href="index.html"><img class="imagenLogo" src="images/logo.jpg"
+            <a href="index.php"><img class="imagenLogo" src="images/logo.jpg"
        alt="HardwareMedia"></a>
-            <a href="index.html"><img class="casita" src="images/casa.png" alt="Inicio"></a>
+            <a href="index.php"><img class="casita" src="images/casa.png" alt="Inicio"></a>
        <nav class="barraSuperior">
         <ul>
             <li><a href="#">Noticias</a>
                 <ul>
-                    <li><a href="juegos.html">Videojuegos</a></li>
-                    <li><a href="hardware.html">Hardware</a></li>
+                    <li><a href="juegos.php">Videojuegos</a></li>
+                    <li><a href="hardware.php">Hardware</a></li>
                 </ul></li>
             <li><a href="#">PC</a>
                 <ul>
-                    <li><a href="mantenimiento.html">Mantenimiento</a></li>
-                    <li><a href="montaje.html">Montaje</a></li>
-                    <li><a href="modding.html">Modding</a></li>
+                    <li><a href="mantenimiento.php">Mantenimiento</a></li>
+                    <li><a href="montaje.php">Montaje</a></li>
+                    <li><a href="modding.php">Modding</a></li>
                     </ul></li>
             <li><a href="#">Información</a>
                 <ul>
-                    <li><a href="nosotros.html">Nosotros</a></li>
-                    <li><a href="contactanos.html">Contáctanos</a></li>
-                    <li><a href="proyectos.html">Proyectos</a></li>
-                    <li><a href="asesoramiento.html">Asesoramiento</a></li>
+                    <li><a href="nosotros.php">Nosotros</a></li>
+                    <li><a href="contactanos.php">Contáctanos</a></li>
+                    <li><a href="proyectos.php">Proyectos</a></li>
+                    <li><a href="asesoramiento.php">Asesoramiento</a></li>
                 </ul></li>
-            <li><a href="tienda.html">Tienda</a></li>    
+            <li><a href="tienda.php">Tienda</a></li>    
             <li><a href="https://twitter.com/hardwaremedia_?lang=es" target="_blank"><img class="twitter" src="images/twitter icon.png"></a></li>
             <li><a href="https://www.instagram.com/hardwaremedia_/?hl=es" target="_blank"><img class="twitter" src="images/instagram.png"></a></li>
         </ul>
@@ -68,19 +68,19 @@
             Aquí mostramos un resumen de todos los sevicios que le ofrecemos:<br>
     
         <div class="iconos">
-        <a href="montaje.html"><img class="imagen3pequeña" src="images/montaje.png"></a>
-        <a href="mantenimiento.html"><img class="imagen3" src="images/mantenimiento.png"></a>
-        <a href="proyectos.html"><img class="imagen3pequeña2" src="images/proyectos.png"></a>
-        <a href="asesoramiento.html"><img class="imagen3" src="images/asesoramiento.png"></a>
-        <a href="modding.html"><img class="imagen3pequeña" src="images/modding.png"></a>
+        <a href="montaje.php"><img class="imagen3pequeña" src="images/montaje.png"></a>
+        <a href="mantenimiento.php"><img class="imagen3" src="images/mantenimiento.png"></a>
+        <a href="proyectos.php"><img class="imagen3pequeña2" src="images/proyectos.png"></a>
+        <a href="asesoramiento.php"><img class="imagen3" src="images/asesoramiento.png"></a>
+        <a href="modding.php"><img class="imagen3pequeña" src="images/modding.png"></a>
         </div>    
-            ¿Quiére conocernos un poco más? <a href="nosotros.html">click aquí</a>.<br>
+            ¿Quiére conocernos un poco más? <a href="nosotros.php">click aquí</a>.<br>
             <br>
-            ¿Desea contactar con nosotros? <a href="contactanos.html">click aquí</a>.<br>
+            ¿Desea contactar con nosotros? <a href="contactanos.php">click aquí</a>.<br>
             <br>
-            Echele un vistazo a nuestra <a href="tienda.html">Tienda</a>.<br>
+            Echele un vistazo a nuestra <a href="tienda.php">Tienda</a>.<br>
             <br>
-            Conozca nuestras noticias de <a href="hardware.html">Hardware</a> y <a href="juegos.html">Videojuegos</a><br>
+            Conozca nuestras noticias de <a href="hardware.php">Hardware</a> y <a href="juegos.php">Videojuegos</a><br>
             <br><br>
             <img class="imagen2" src="images/imagenNosotros.jpg"><br>
             <img class="contadorVisitas" src="https://counter9.wheredoyoucomefrom.ovh/private/contadorvisitasgratis.php?c=rmnq2q8qbh3441dhct2jqemhdxbd11yw" border="0" alt="contador de visitas"><br>
@@ -92,4 +92,4 @@
 </div>
 
 </body>
-</html>
+</php>
